3bc108ea9e9d48cc060286fcd5c3d3d5a50781dd
karakRohan <karakrohan2005@gmail.com>
2025-02-01 12:02:53 +0530

    7th Changes

--- 1_Calender.c
+++ 1_Calender.c
@@ -15,6 +15,7 @@ int main()
     if (month == 2)
     {
         // Check for leap year
+        
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
             daysInMonth = 29;
         else
